05bbb1997b004b2b944dcfa533f5bca172cd1c99
Ourobè Donatien <cdonatien9@gmail.com>
2023-08-20 15:24:40 +0000

    Implementation des tests pour la connexion d'un utilisateur

--- tests_fonctions.py
+++ tests_fonctions.py
@@ -6,7 +6,7 @@ import utilisateurs
 import verifier
 
 # Fonction de test pour la création de la table des utilisateurs
-def test_create_table_utilisateurs():
+def test_inscription_utilisateurs():
 
     utilisateurs.create_table_utilisateurs() # Création de la table
 
@@ -21,6 +21,19 @@ def test_create_table_utilisateurs():
     # Cas 1 : test de l'inscription d'un utilisateur existant
     # Verifier si l'insertion dans la BD a échoué
     utilisateurs.enregistrement_utilisateurs(nom_utilisateur, email, password, numero) is None
+    
+# Appel de la fonction
+test_inscription_utilisateurs()
 
-test_create_table_utilisateurs()
+# Fonction de test pour la création de la table des utilisateurs
+def test_connexion():
+    # Cas 1 : Test pour une connexion réussie
+    nom_utilisateur = "donatien"
+    password = "donaCO1234"
+    login.login(nom_utilisateur, password) is True
 
+    # Cas 2 : Test pour une connexion échouée
+    login.login(nom_utilisateur, password) is None
+
+# Appel de la fonction
+test_connexion()
